4e8999623f81455ce6cb0f2e58d517dcd4e10299
OBrandonC <cirobrandon555@gmail.com>
2026-04-24 08:52:39 -0500

    agrego contenido a local-storage

--- src/helpers/local-storage.js
+++ src/helpers/local-storage.js
@@ -1,1 +1,11 @@
+const KEY = 'reservasBonsai';
 
+export const guardarReserva = (formData) => {
+  const reservas = obtenerReservas();
+  reservas.push(formData);
+  localStorage.setItem(KEY, JSON.stringify(reservas));
+};
+
+export const obtenerReservas = () => {
+  return JSON.parse(localStorage.getItem(KEY)) || [];
+};
