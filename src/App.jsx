94e54aa035f2a7222738d9b4c26021a2f47bcf65
OBrandonC <cirobrandon555@gmail.com>
2026-04-24 08:59:32 -0500

    corrección de informaciones de App.jsx
    Refactored routing to use routerApp for dynamic route mapping.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import FilosofiaPage from './pages/FilosofiaPage';
-import GaleriaPage from './pages/GaleriaPage';
-import ReservasPage from './pages/ReservasPage';
+import { routerApp } from './router/routerApp';
 import './App.css';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/filosofia" element={<FilosofiaPage />} />
-        <Route path="/galeria" element={<GaleriaPage />} />
-        <Route path="/reservas" element={<ReservasPage />} />
+        {routerApp.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
       </Routes>
     </Router>
   );
